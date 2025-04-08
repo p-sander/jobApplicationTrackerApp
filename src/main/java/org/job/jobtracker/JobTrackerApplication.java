eb865bb05b97e56840b6d036602685acbb0d4af7
Patrycja Sander <patrycja.sander2@gmail.com>
2025-04-08 11:19:43 +0200

    initial

---
+++ src/main/java/org/job/jobtracker/JobTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.job.jobtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/job/jobtracker/JobTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.job.jobtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
