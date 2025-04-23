6f8399a8f72992cabafd07cc0647f5954580b7a8
Patrycja Sander <patrycja.sander2@gmail.com>
2025-04-23 10:06:25 +0200

    basic setup - index.html, properties, dependencies, styles

---
+++ src/main/java/org/job/jobtracker/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package org.job.jobtracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(withDefaults())
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/**").permitAll(); //
+        return http.build();
+    }
+
+
+}
+
